42e590b823adaab6a1f783145f5f0e54c8318a1e
monsieursquirrel <conrad@bebbington.org>
2015-09-19 17:24:34 +0100

    create an oscillator struct

--- src/main.rs
+++ src/main.rs
@@ -13,57 +13,20 @@ use std::f32::consts::PI;
 use itertools::Zip;
 use std::sync::mpsc;
 
-const SHIFT: f32 = (1 << 16) as f32;
-
-struct PhaseIter {
-    current: u32,
-    loop_len: u32,
-    outscale: f32,
-}
-
-impl PhaseIter {
-    fn calc_loop(freq: f32, sample_rate: u32) -> u32 {
-        (((sample_rate as f32) * SHIFT) / (freq)) as u32
-    }
-
-    fn new(freq: f32, sample_rate: u32, outscale: f32) -> Self {
-        PhaseIter {
-            current: 0,
-            loop_len: calc_loop(freq, sample_rate),
-            outscale: outscale
-        }
-    }
-
-    fn set_freq(&mut self, freq: f32, sample_rate: u32) {
-        // need to try to avoid jumps in output
-        let new_len = calc_loop(freq, sample_rate);
-        let new_pos = (self.current * new_len) / self.loop_len;
-        self.loop_len = new_len;
-        self.current = new_pos;
-    }
-}
-
-impl Iterator for PhaseIter {
-    type Item = f32;
-    fn next(&mut self) -> Option<f32> {
-        let pos = self.current;
-        self.current = (self.current + (1 << 16)) % self.loop_len;
-        Some(((pos as f32) * self.outscale) / (self.loop_len as f32))
-    }
-}
+mod oscillator;
 
 fn main() {
 
     let freq = Step(57.0).to_hz().hz();
     // generate harmonics
-    let h0 = PhaseIter::new(freq * 1.0, 44_100, PI * 2.0).map(|phase| phase.sin() as f32);
-    let h1 = PhaseIter::new(freq * 2.0, 44_100, PI * 2.0).map(|phase| phase.sin() as f32);
-    let h2 = PhaseIter::new(freq * 3.0, 44_100, PI * 2.0).map(|phase| phase.sin() as f32);
-    let h3 = PhaseIter::new(freq * 4.0, 44_100, PI * 2.0).map(|phase| phase.sin() as f32);
-    let h4 = PhaseIter::new(freq * 5.0, 44_100, PI * 2.0).map(|phase| phase.sin() as f32);
-    let h5 = PhaseIter::new(freq * 6.0, 44_100, PI * 2.0).map(|phase| phase.sin() as f32);
-    let h6 = PhaseIter::new(freq * 7.0, 44_100, PI * 2.0).map(|phase| phase.sin() as f32);
-    let h7 = PhaseIter::new(freq * 8.0, 44_100, PI * 2.0).map(|phase| phase.sin() as f32);
+    let h0 = oscillator::Oscillator::new(freq * 1.0, 44_100);
+    let h1 = oscillator::Oscillator::new(freq * 2.0, 44_100);
+    let h2 = oscillator::Oscillator::new(freq * 3.0, 44_100);
+    let h3 = oscillator::Oscillator::new(freq * 4.0, 44_100);
+    let h4 = oscillator::Oscillator::new(freq * 5.0, 44_100);
+    let h5 = oscillator::Oscillator::new(freq * 6.0, 44_100);
+    let h6 = oscillator::Oscillator::new(freq * 7.0, 44_100);
+    let h7 = oscillator::Oscillator::new(freq * 8.0, 44_100);
 
     // mix them
     let mut mixed = Zip::new((h0, h1, h2, h3, h4, h5, h6, h7))
@@ -90,14 +53,14 @@ fn main() {
                 match message {
                     Ok(pitch) => {
                         // update pitch
-                        
+
                     }
                     Err(mpsc::TryRecvError::Empty) => {
                         break;
                     }
                     Err(mpsc::TryRecvError::Disconnected) => {
                         // get out of here
-                        return Err(());
+                        return Err("EOF".to_owned());
                     }
                 }
             }
---
+++ src/oscillator.rs
@@ -0,0 +1,65 @@
+use num::Float;
+use num::traits::Num;
+use std::f32::consts::PI;
+
+const SHIFT: f32 = (1 << 16) as f32;
+
+struct PhaseIter {
+    pos: u32,
+    loop_len: u32,
+    outscale: f32,
+}
+
+impl PhaseIter {
+    fn calc_loop(freq: f32, sample_rate: u32) -> u32 {
+        (((sample_rate as f32) * SHIFT) / (freq)) as u32
+    }
+
+    fn new(freq: f32, sample_rate: u32, outscale: f32) -> Self {
+        PhaseIter {
+            pos: 0,
+            loop_len: Self::calc_loop(freq, sample_rate),
+            outscale: outscale
+        }
+    }
+
+    fn set_freq(&mut self, freq: f32, sample_rate: u32) {
+        // try to avoid jumps in output by remapping the current position to the new loop len
+        let new_len = Self::calc_loop(freq, sample_rate);
+        let new_pos = (self.pos * new_len) / self.loop_len;
+        self.loop_len = new_len;
+        self.pos = new_pos;
+    }
+}
+
+impl Iterator for PhaseIter {
+    type Item = f32;
+    fn next(&mut self) -> Option<f32> {
+        let pos = self.pos;
+        self.pos = (self.pos + (1 << 16)) % self.loop_len;
+        Some(((pos as f32) * self.outscale) / (self.loop_len as f32))
+    }
+}
+
+pub struct Oscillator {
+    phase: PhaseIter
+}
+
+impl Oscillator {
+    pub fn new(freq: f32, sample_rate: u32) -> Self {
+        Oscillator {
+            phase: PhaseIter::new(freq, sample_rate, PI * 2.0)
+        }
+    }
+
+    pub fn set_freq(&mut self, freq: f32, sample_rate: u32) {
+        self.phase.set_freq(freq, sample_rate);
+    }
+}
+
+impl Iterator for Oscillator {
+    type Item = f32;
+    fn next(&mut self) -> Option<f32> {
+        self.phase.next().map(|phase| phase.sin())
+    }
+}

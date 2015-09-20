1b352d9fd11ad285bb5411b0228b1417c369ca92
monsieursquirrel <conrad@bebbington.org>
2015-09-20 13:54:20 +0100

    drop out harmonics above nyquist limit

--- src/oscillator.rs
+++ src/oscillator.rs
@@ -24,7 +24,7 @@ impl PhaseIter {
     }
 
     fn set_freq(&mut self, freq: f32, sample_rate: u32) {
-        if freq > 0.0 {
+        if freq > 0.0 && freq < (sample_rate as f32 / 2.0) {
             // try to avoid jumps in output by remapping the current position to the new loop len
             let new_len = Self::calc_loop(freq, sample_rate);
             let new_pos = (((self.pos as u64) * (new_len as u64)) / (self.loop_len as u64)) as u32;
@@ -32,8 +32,7 @@ impl PhaseIter {
             self.pos = new_pos;
         }
         else {
-            // avoid divide by zero
-            self.loop_len = 1;
+            self.loop_len = 0;
             self.pos = 0;
         }
     }
@@ -42,9 +41,14 @@ impl PhaseIter {
 impl Iterator for PhaseIter {
     type Item = f32;
     fn next(&mut self) -> Option<f32> {
-        let pos = self.pos;
-        self.pos = (self.pos + (1 << 16)) % self.loop_len;
-        Some(((pos as f32) * self.outscale) / (self.loop_len as f32))
+        if self.loop_len > 0 {
+            let pos = self.pos;
+            self.pos = (self.pos + (1 << 16)) % self.loop_len;
+            Some(((pos as f32) * self.outscale) / (self.loop_len as f32))
+        }
+        else {
+            Some(0.0)
+        }
     }
 }
 
